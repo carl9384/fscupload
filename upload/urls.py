71b66b00164d74245ab8f5038f9c0676a51b7554
Carl <cnegro@nysbc.org>
2017-08-01 18:59:11 -0400

    Add .gitignore

--- upload/urls.py
+++ upload/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from upload import views as uploadviews
 from django.contrib import admin
 
@@ -7,8 +9,7 @@ urlpatterns =[
     url(r'^$', uploadviews.index, name='index'),
     url(r'^list/$', uploadviews.list, name='list'),
     url(r'^uploadcomplete/$',uploadviews.uploadcomplete, name='uploadcomplete'),
-    url(r'^admin/', include(admin.site.urls),name='uploadadmin'),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
     #('upload.views',
