ba701de6d5714d6a3cdd4cd94cedd423d73af381
Carl Negro <carlnegro@gmail.com>
2017-08-04 14:48:59 -0400

    Remove decorator_include import

--- fscupload/urls.py
+++ fscupload/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from decorator_include import decorator_include
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
