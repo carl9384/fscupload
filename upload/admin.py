df560cbea107e84b81c6fab4d8b739ff5202a2f6
Carl Negro <carlnegro@gmail.com>
2018-03-06 19:49:16 -0500

    update upload app

--- upload/admin.py
+++ upload/admin.py
@@ -7,6 +7,6 @@ from upload.models import Fscjob
 @admin.register(Fscjob)
 class FscjobAdmin(admin.ModelAdmin):
     list_display = ['jobname','status','halfmap1_name','halfmap2_name','completefile_name','created_date']
-    readonly_fields=('password','uniquefolder','apix','coneangle','fsccutoff','sphericitythresh','highpassfilter','halfmap1file','halfmap2file','fullmapfile','maskfile','emailaddress','completefile','jobname','user','task_id','status')
-    fields = ('jobname','status','task_id','user','emailaddress','completefile','password','halfmap1file','halfmap2file','maskfile','apix','coneangle','fsccutoff','sphericitythresh','highpassfilter')
+    readonly_fields=('id','password','uniquefolder','apix','coneangle','fsccutoff','sphericitythresh','highpassfilter','halfmap1file','halfmap2file','fullmapfile','maskfile','emailaddress','completefile','jobname','user','task_id','status')
+    fields = ('id','jobname','status','task_id','user','emailaddress','completefile','password','halfmap1file','halfmap2file','maskfile','apix','coneangle','fsccutoff','sphericitythresh','highpassfilter')
 
