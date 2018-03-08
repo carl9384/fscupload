d16315161611a55b51109e8bc167793b25f36fb3
Carl Negro <carlnegro@gmail.com>
2018-03-07 22:07:13 -0500

    add apix validation

--- upload/forms.py
+++ upload/forms.py
@@ -6,6 +6,7 @@ class FscjobForm(forms.ModelForm):
 
     honeypot = forms.CharField(widget=forms.HiddenInput(), required=False)
     jobname = forms.CharField(min_length=5,max_length=40, required=True)
+    apix = forms.FloatField(max_value=3.0,required=True)
 
     class Meta:
         model = Fscjob
