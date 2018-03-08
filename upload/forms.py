e70d743e828c0d27b2a461d73928763e56229a4d
Carl Negro <carlnegro@gmail.com>
2018-03-07 21:22:55 -0500

    remove old function from upload.forms

--- upload/forms.py
+++ upload/forms.py
@@ -14,23 +14,3 @@ class FscjobForm(forms.ModelForm):
         exclude = ['emailaddress','uniquefolder','password','completefile','user','histogram_file','ftplot_file','plots_file','task_id']
         widgets = { 'apix': forms.TextInput({'size': 10})}
 
-
-    def send_email2(self,job_id):
-        print('inside send_email function')
-        # try to trick spammers by checking whether the honeypot field is
-        # filled in; not super complicated/effective but it works
-        if self.cleaned_data['honeypot']:
-            return False
-
-        job = Fscjob.objects.get(pk=job_id)
-
-        message = "Hi,\n \
-                  Your 3DFSC job %s is now being processed.\n \
-                  You will receive an email when it is complete with a download link.\n\n  \
-                  Thanks,\n \
-                  \n\n \
-                  YTZ, PRB, DL" %(job.jobname)
-
-        send_upload_email_task.delay(
-            self.cleaned_data['emailaddress'],message,job_id)
-
