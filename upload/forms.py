b64ec75e7384ebafc8498c2fcb40152c1e15b5e1
Carl Negro <carlnegro@gmail.com>
2018-06-08 15:05:42 -0400

    add validator to all map fields

--- upload/forms.py
+++ upload/forms.py
@@ -4,8 +4,8 @@ from django.core.validators import FileExtensionValidator
 
 from upload.models import Fscjob
 
-no_space_validator = RegexValidator(r'^[^\s]+$',message='No spaces allowed',code='invalid_username')
-alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+no_space = RegexValidator(r'^[^\S]+$',message='No spaces allowed')
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$',message='Only alphanumeric characters are allowed.')
 fileextension = FileExtensionValidator(['mrc','map'],message='Only .mrc and .map files can be processed. Please convert your maps to those format.')
 
 class FscjobForm(forms.ModelForm):
@@ -13,6 +13,9 @@ class FscjobForm(forms.ModelForm):
     jobname = forms.CharField(min_length=5,max_length=40, required=True,validators=[alphanumeric])
     apix = forms.FloatField(min_value=0.00001,max_value=100.0,required=True)
     halfmap1file = forms.FileField(required=True,validators=[fileextension])
+    halfmap2file = forms.FileField(required=True,validators=[fileextension])
+    fullmapfile = forms.FileField(required=True,validators=[fileextension])
+    maskfile = forms.FileField(required=False,validators=[fileextension])
 
 
     class Meta:
